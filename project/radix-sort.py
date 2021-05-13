d87d0bc01e351b2e59c0997e79cecd0ba6fb97dd
221ChungJ <jeremychung2022@gmail.com>
2021-05-12 23:36:48 -0500

    Finished Project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,5 +1,5 @@
-import urllib
-import requests
+from unittest import TestCase   
+import urllib.request
 
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     booktxt = urllib.request.urlopen(book_url).read().decode()
@@ -7,4 +7,37 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    arr_of_words = book_to_words(book_url)
+
+    longestWord = len(max(arr_of_words, key=len))
+    
+    for i in range(longestWord):
+        arr_of_words = countSort(arr_of_words, i, longestWord-1)
+
+    return arr_of_words
+    
+
+def countSort(arr, index, max_len):
+    output = [0 for i in range(len(arr))]
+
+    count = [0 for i in range(128)]
+
+    for word in arr:
+        if max_len - index < len(word):
+            count[word[max_len - index]] += 1
+        else:
+            count[0] += 1
+    
+    for i in range(128):
+        count[i] += count[i-1]
+    
+    for i in range(len(arr)-1, -1, -1):
+        if max_len - index < len(arr[i]):
+            output[count[arr[i][max_len - index]]-1] = arr[i]
+            count[arr[i][max_len - index]] -= 1
+        else:
+            output[count[0] - 1] = arr[i]
+            count[0] -= 1 
+    
+    return output
+
